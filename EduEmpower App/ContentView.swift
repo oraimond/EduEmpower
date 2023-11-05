0e0d4b3fb5bfa8797f2cffd7636ad1b7107f32f3
Oli Raimond <oraimond@umich.edu>
2023-11-05 12:19:13 -0500

    Initial Commit

---
+++ EduEmpower App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EduEmpower App
+//
+//  Created by Oli Raimond on 11/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EduEmpower App/EduEmpower_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduEmpower_AppApp.swift
+//  EduEmpower App
+//
+//  Created by Oli Raimond on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct EduEmpower_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
