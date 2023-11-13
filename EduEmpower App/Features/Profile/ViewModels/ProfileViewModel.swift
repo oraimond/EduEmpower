f98e9e98a2e150f1cd96ecb610c1ad9897a3c05f
Vanessa Kang <sorikang@umich.edu>
2023-11-13 12:26:12 -0500

    Profile view done

--- EduEmpower App/Features/Profile/ViewModels/ProfileViewModel.swift
+++ EduEmpower App/Features/Profile/ViewModels/ProfileViewModel.swift
@@ -5,4 +5,13 @@
 //  Created by Sori Kang on 11/13/23.
 //
 
-import Foundation
+import SwiftUI
+
+class ProfileViewModel: ObservableObject {
+    
+    var buttonColor: Color {
+        return Color.blue
+    }
+    
+    let dummyUser: User = User(fname: "John", lname: "Doe")
+}
--- EduEmpower App/Features/Profile/Views/ProfileView.swift
+++ EduEmpower App/Features/Profile/Views/ProfileView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @ObservedObject var viewModel: ProfileViewModel = ProfileViewModel()
+    
+    @State private var logOutActive = false
+    
     var body: some View {
-        Text("Profile View")
+        NavigationStack {
+            VStack {
+                Spacer()
+                Image(systemName: "person.circle.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 100, height: 100)
+                                    .clipShape(Circle())
+                                    .foregroundColor(.blue)
+                Text(viewModel.dummyUser.fname).font(.largeTitle)
+                Text(viewModel.dummyUser.lname).font(.largeTitle)
+                Spacer()
+                Button(
+                    action: {logOutActive = true},
+                    label: {
+                        Text("Logout")
+                            .font(.system(size: 24, weight: .bold, design: .default))
+                            .frame(maxWidth: .infinity, maxHeight: 60)
+                            .foregroundColor(Color.white)
+                            .background(viewModel.buttonColor)
+                            .cornerRadius(10)
+                    }
+                )
+                .navigationDestination(isPresented: $logOutActive) {
+                    LoginView()
+                }
+                Spacer()
+            }
+        }
     }
 }
 
